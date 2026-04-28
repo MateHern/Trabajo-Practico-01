fa7d3dec9736e06921dcb62a4f1ae16814f230e4
MATEO HERNANDEZ KRASUK <49979691@ort>
2026-04-28 09:02:18 -0300

    Corriguiendo errores

--- Models/Integrante.cs
+++ Models/Integrante.cs
@@ -1,44 +1,28 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace Tp01.Models
+{
+    public class Integrante
+    {
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Foto { get; set; }
+        public List<string> Familiares { get; set; }
+        public List<string> Intereses { get; set; }
+
+        public Integrante(int dni, string nombre, DateTime fechaNacimiento, string foto, List<string> familiares, List<string> intereses)
+        {
+            DNI = dni;
+            Nombre = nombre;
+            FechaNacimiento = fechaNacimiento;
+            Foto = foto;
+            Familiares = familiares;
+            Intereses = intereses;
+        }
+    }
+}
 namespace Tp01;
 
-public class Integrante
-{
-    private int dni;
-    private string nombre;
-    private DateTime fechaDeNacimiento;
-    private string foto;
-    private List<string> Familiares;
-    private List<string>  Intereses;
-
-    public Integrante(int dni, string nombre, DateTime fechaDeNacimiento, string foto, List <string> Familiares, List<string> Intereses){
-        this.dni= dni;
-        this.nombre = nombre;
-        this.fechaDeNacimiento = fechaDeNacimiento;
-        this.foto = foto;
-        this.Familiares = Familiares;
-        this.Intereses = Intereses;
-    }
-
-    public string ObtenerNombre(){
-        return nombre;
-    }
-
-    public int ObtenerDni(){
-        return dni;
-    }
-
-    public DateTime ObtenerFechadeNacimiento(){
-        return fechaDeNacimiento;
-    }
-
-    public string Obtenerfoto(){
-        return foto;
-    }
-
-    public List<string> ObtenerFamiliares(){
-        return Familiares;
-    }
-    public List<string> ObtenerIntereses(){
-        return Intereses;
-    }
-
-}
