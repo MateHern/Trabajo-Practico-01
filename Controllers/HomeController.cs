87d326f6bb9a6203db6fbcb2fbb6710055d4d2b3
MATEO HERNANDEZ KRASUK <49979691@ort>
2026-05-06 09:30:28 -0300

    triple

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,39 +1,3 @@
-// using System.Diagnostics;
-// using Microsoft.AspNetCore.Mvc;
-// using Tp01.Models;
-
-// namespace Tp01.Controllers;
-
-// public class HomeController : Controller
-// {
-//     private readonly ILogger<HomeController> _logger;
-
-//     public HomeController(ILogger<HomeController> logger)
-//     {
-//         _logger = logger;
-//     }
-
-//     public IActionResult Index()
-//     {
-//         Grupo grupo = new Grupo ();
-//         Dictionary<int,Integrante> pablo= grupo.mostrarIntegrantes();
-//         ViewBag.Integrantes=pablo;
-
-//         return View();
-//     }
-
-//     public IActionResult Privacy()
-//     {
-//         return View();
-//     }
-
-//     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-//     public IActionResult Error()
-//     {
-//         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-//     }
-// }
-
 using Microsoft.AspNetCore.Mvc;
 using Tp01.Models;
 
@@ -44,7 +8,7 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         Grupo grupo = new Grupo();
-        ViewBag.Integrantes = grupo.mostrarIntegrantes();
+        ViewBag.Integrantes = grupo.DevolverIntegrantes();
         return View();
     }
 
--- Models/Grupo.cs
+++ Models/Grupo.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-
 namespace Tp01.Models
 {
     public class Grupo
@@ -13,7 +12,7 @@ namespace Tp01.Models
             CargaDatosManual();
         }
 
-        public Dictionary<int, Integrante> mostrarIntegrantes()
+        public Dictionary<int, Integrante> DevolverIntegrantes()
         {
             return Integrantes;
         }
@@ -21,32 +20,19 @@ namespace Tp01.Models
         public Integrante GetIntegrante(int dni)
         {
             if (Integrantes.ContainsKey(dni))
-            {
                 return Integrantes[dni];
-            }
             return null;
         }
 
         public void CargaDatosManual()
         {
-            List<string> familiaresDuv = new List<string>();
-            familiaresDuv.Add("Papá: Juan");
-            familiaresDuv.Add("Mamá: Dolores");
+            List<string> familiaresDuv = new List<string> { "Papá: Juan", "Mamá: Dolores" };
+            List<string> familiaresHern = new List<string> { "Papá: Rodolfo", "Mamá: Cintia" };
+            List<string> interesesDuv = new List<string> { "Hacer mandalas", "Hacer yoga" };
+            List<string> interesesHern = new List<string> { "Ser plomero", "Cortar leña" };
 
-            List<string> familiaresHern = new List<string>();
-            familiaresHern.Add("Papá: Rodolfo");
-            familiaresHern.Add("Mamá: Cintia");
-
-            List<string> interesesDuv = new List<string>();
-            interesesDuv.Add("Hacer mandalas");
-            interesesDuv.Add("Hacer yoga");
-
-            List<string> interesesHern = new List<string>();
-            interesesHern.Add("Ser plomero");
-            interesesHern.Add("Cortar leña");
-
-            Integrante integrante1 = new Integrante(49764404, "Matias", new DateTime(2009, 09, 30), "mati12.jpg", familiaresDuv, interesesDuv);
-            Integrante integrante2 = new Integrante(49979691, "Mateo", new DateTime(2009, 12, 13), "Sin título.jpg", familiaresHern, interesesHern);
+            Integrante integrante1 = new Integrante(49764404, "Matias", new DateTime(2009, 09, 30), "/img/indio.jpg", familiaresDuv, interesesDuv);
+            Integrante integrante2 = new Integrante(49979691, "Mateo", new DateTime(2009, 12, 13), "/img/valentini.jpg", familiaresHern, interesesHern);
 
             Integrantes.Add(49764404, integrante1);
             Integrantes.Add(49979691, integrante2);
